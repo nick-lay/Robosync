dcf62f431234cef72810f62ee8c6695c6c8c7c9f
Nickolay Demin <demin.n.m@yandex.ru>
2023-10-31 20:57:57 +0700

    function CheckMagic

--- Robosync.ps1
+++ Robosync.ps1
@@ -5,11 +5,25 @@
 
 
 # Magic world
-#Set-Variable MAGIC -Value "MAGIC"
+Set-Variable MAGIC -Value "MAGIC"
 
 # Source path
-#Set-Variable Source -Value ""
+Set-Variable Source -Value ""
 
 # Destination path
-#Set-Variable Dest -Value "./"
+Set-Variable Dest -Value "./"
 
+
+function CheckMagic () {
+    if (($MAGIC -eq $null) -or ($MAGIC -eq "")) {
+        Write-Output "Set MAGIC word"
+        Exit
+    }
+}
+
+
+CheckMagic
+
+
+
+Write-Output ""
